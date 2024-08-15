f3d6583a66083d961a46d118642fbf143f35ea1e
Ttwinkleee <103359772+anstdud@users.noreply.github.com>
2024-08-15 14:54:42 +0300

    Add files via upload

--- mi-electro.js
+++ mi-electro.js
@@ -1,6 +1,16 @@
-// function NewTab() {
-//     window.open( "reviews-window/reviews-mi-electro.html");
-// } 
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 1,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
 const callback = () => {
     window.open("reviews-mi-electro.html");
 };
